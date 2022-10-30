bcf574efa2ed6275dbfcde16886571ea594f7d1a
hideki-okawa <h.okawa0303@gmail.com>
2022-10-30 11:50:14 +0900

    コメントの追加

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   private
 
     def sign_up_params
+      # Strong Parameters
+      # email, password, password_confirmation, nameは必須
       params.permit(:email, :password, :password_confirmation, :name)
     end
 end
